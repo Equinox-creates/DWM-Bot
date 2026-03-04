0f4c7cdc194c2a5af839d8a7d0b7e1f779dadf8f
Equinox creators <PICKLUROYCHOWDHURY2@GMAIL.COM>
2026-03-04 12:01:37 +0530

    Add basic Discord bot with kick command

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+# Northflank gets the token from your "Environment Variables"
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason="No reason"):
+    await member.kick(reason=reason)
+    await ctx.send(f'Kicked {member.mention}!')
+
+bot.run(TOKEN)
